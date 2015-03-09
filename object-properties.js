8c15043c6705d1328c3474e965f2148239d002bd
gachua <hieumai3@gmail.com>
2015-03-09 11:11:19 +0700

    dasd

---
+++ object-properties.js
@@ -0,0 +1,9 @@
+var food = {
+	types: 'only pizza'
+}
+
+var a = food['types'] //make a reference to a object's property
+var b = food.types
+console.log(food.types)
+console.log(a)
+console.log(b)
--- readme.txt
+++ readme.txt
@@ -1,2 +1,3 @@
 i'm learning code probarly
 write another line here
+123
